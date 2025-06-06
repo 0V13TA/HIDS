1d628c2bcc44cdf7fd0e0c7d327ec52628dcdd87
0V13TA <vincentpaulomorogbe6@gmail.com>
2025-06-06 12:01:50 +0000

    Jun 6, 2025, 1:01 PM

--- gui/main.py
+++ gui/main.py
@@ -1,12 +1,14 @@
 from PySide6.QtWidgets import QApplication
 import sys
-from ui.main_window import MainWindow
+from gui.ui.main_window import MainWindow
+
 
 def main():
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
+
 
 if __name__ == "__main__":
     main()
