d240d7029e0e42142fd83dd400aed64e4c96e52f
0V13TA <vincentpaulomorogbe6@gmail.com>
2025-06-06 11:53:58 +0100

    Refactor main application logic: initialize database, manage hash generation state, and schedule hash comparison jobs

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
-from config import HIDS_WATCH_DIRECTORY
-from utils.hash_comparator import generate_file_hashes
+import time
+import schedule
+from utils.hash_db import init_db
+from config import HIDS_WATCH_DIRECTORY, HIDS_IS_HASHED
+from utils.hash_comparator import generate_file_hashes, compare_all_hashes
 
-print(generate_file_hashes(HIDS_WATCH_DIRECTORY))
+if __name__ == "__main__":
+    init_db()
+
+if not HIDS_IS_HASHED:
+    generate_file_hashes(HIDS_WATCH_DIRECTORY)
+    HIDS_IS_HASHED = True
+
+
+def job() -> None:
+    if not compare_all_hashes(HIDS_WATCH_DIRECTORY):
+        print("Hash comparison failed.")
+    else:
+        print("Hash comparison successful.")  # pragma: no cover
+
+
+# Schedule the job every 10 minutes
+schedule.every(5).seconds.do(job)
+
+print("HIDS is running. Press Ctrl+C to exit.")
+job()  # Run once at startup
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
--- utils/logger.py
+++ utils/logger.py
@@ -1,7 +1,6 @@
 import os
-import json
 from datetime import datetime
-from config import HIDS_LOG_FOLDER, HIDS_HASHES_FILE
+from config import HIDS_LOG_FOLDER
 
 
 def print_to_file(message: str, ty: str) -> None:
@@ -12,8 +11,3 @@ def print_to_file(message: str, ty: str) -> None:
     log_entry = f"[{time_str}] [{ty.upper()}] {message}"
     with open(log_path, "a") as f:
         f.write(log_entry + "\n")
-
-
-def write_hashes(hash_dict: dict[str, str]):
-    with open(HIDS_HASHES_FILE, "w") as f:
-        json.dump(hash_dict, f)
