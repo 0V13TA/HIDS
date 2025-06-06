9709962420059e9a78bb1b5fe113a2c14b123c38
0V13TA <vincentpaulomorogbe6@gmail.com>
2025-06-06 12:17:00 +0100

    Fix HIDS_HASHES_FILE path to reference HIDS_WATCH_DIRECTORY correctly

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 HIDS_WATCH_DIRECTORY = "/home/ovieta/Documents/cybersecurity_notes/code/HIDS/secure"
 HIDS_LOG_FOLDER = f"{HIDS_WATCH_DIRECTORY}/.config/hids_log/"
-HIDS_HASHES_FILE = f"{HIDS_LOG_FOLDER}/.config/hids_hashes.db"
+HIDS_HASHES_FILE = f"{HIDS_WATCH_DIRECTORY}/.config/hids_hashes.db"
 HIDS_IS_HASHED = False
